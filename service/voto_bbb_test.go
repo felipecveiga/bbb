aeb33efaa4a00d4b85f60669d727ff5bd283c4a5
Felipe veiga <fcosta.veiga@gmail.com>
2025-04-02 20:39:33 -0300

    feat: implementa testes unitários casos de erros

--- service/voto_bbb_test.go
+++ service/voto_bbb_test.go
@@ -56,3 +56,52 @@ func TestCreateVote_WhenConsultingParticipant_ReturnError(t *testing.T) {
 
 	assert.Error(t, err)
 }
+
+func TestCreateVote_WhenConsultingStatusParticipant_ReturnError(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	mockRepo := repository.NewMockRepository(ctrl)
+	service := NewService(mockRepo)
+
+	now := time.Now()
+
+	vote := &model.HistoricoVoto{
+		ID:             1,
+		IdParticipante: 2,
+		Ip:             "",
+		Created_at:     now,
+	}
+
+	mockRepo.EXPECT().
+		GetParticipantFomDB(vote.IdParticipante).
+		Return(true, nil)
+	mockRepo.EXPECT().
+	GetParticipantStatusFromDB(vote.IdParticipante).
+		Return(nil, errors.New("erro: ao consultar status"))
+
+	err := service.CreateVote(vote)
+
+	assert.Error(t, err)
+}
+
+func TestCreateVote_WhenReturnError(t *testing.T) {
+	ctrl := gomock.NewController(t)
+
+	mock := NewMockService(ctrl)
+
+	now := time.Now()
+
+	vote := &model.HistoricoVoto{
+		ID:             1,
+		IdParticipante: 2,
+		Ip:             "",
+		Created_at:     now,
+	}
+
+	mock.EXPECT().
+		CreateVote(vote).
+		Return(errors.New("erro ao registrar voto"))
+
+	err := mock.CreateVote(vote)
+
+	assert.Error(t, err)
+}
