601449a12b1f39e7d66bfbb7287573b533c4f4ab
Felipe veiga <fcosta.veiga@gmail.com>
2025-04-02 20:11:40 -0300

    feat: implementa config e testes unitários

--- service/voto_bbb_test.go
+++ service/voto_bbb_test.go
@@ -1,15 +1,58 @@
 package service
 
 import (
+	"errors"
 	"testing"
+	"time"
 
+	"github.com/felipecveiga/bbb/model"
+	"github.com/felipecveiga/bbb/repository"
+	"github.com/stretchr/testify/assert"
 	"go.uber.org/mock/gomock"
 )
 
-func Test_CreateVoto_WhenReturn_Sucess(t *testing.T) {
-ctrl := gomock.NewController(t)
+func TestCreateVote_WhenReturnSucess(t *testing.T) {
+	ctrl := gomock.NewController(t)
 
-m := NewMockService(ctrl)
+	mock := NewMockService(ctrl)
 
-m.EXPECT()
+	now := time.Now()
+
+	vote := &model.HistoricoVoto{
+		ID:             1,
+		IdParticipante: 2,
+		Ip:             "",
+		Created_at:     now,
+	}
+
+	mock.EXPECT().
+		CreateVote(vote).
+		Return(nil)
+
+	err := mock.CreateVote(vote)
+
+	assert.NoError(t, err)
+}
+
+func TestCreateVote_WhenConsultingParticipant_ReturnError(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	mockRepo := repository.NewMockRepository(ctrl)
+	service := NewService(mockRepo)
+
+	now := time.Now()
+
+	vote := &model.HistoricoVoto{
+		ID:             1,
+		IdParticipante: 2,
+		Ip:             "",
+		Created_at:     now,
+	}
+
+	mockRepo.EXPECT().
+		GetParticipantFomDB(vote.IdParticipante).
+		Return(false, errors.New("erro: ao consultar participante"))
+
+	err := service.CreateVote(vote)
+
+	assert.Error(t, err)
 }
